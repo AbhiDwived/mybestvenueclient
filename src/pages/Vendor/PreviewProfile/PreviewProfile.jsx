cbefcae90a2c83d472f00e3be98c659e702cd351
AbhiDwivedi <abhidwivedi687@gmail.com>
2025-05-29 12:05:09 +0530

    feat: Uncomment image imports and display images in PreviewProfile component

--- src/pages/Vendor/PreviewProfile/PreviewProfile.jsx
+++ src/pages/Vendor/PreviewProfile/PreviewProfile.jsx
@@ -4,9 +4,9 @@ import { FiPhone, FiGlobe, FiCalendar } from "react-icons/fi";
 import { HiOutlineMail } from "react-icons/hi";
 import { IoLocationOutline } from 'react-icons/io5';
 
-// import PreviewProfileImg from "../../../assets/PreviewProfile.png";
-// import vendorManagement003 from "../../../assets/vendorManagement003.png";
-// import previewProfile003 from "../../../assets/previewProfile003.png";
+import PreviewProfileImg from "../../../assets/PreviewProfile.png";
+import vendorManagement003 from "../../../assets/vendorManagement003.png";
+import previewProfile003 from "../../../assets/previewProfile003.png";
 
 import PreviewProfileScreen from './PreviewProfileScreen';
 import CustomerReviews from './CustomerReviews';
@@ -44,12 +44,12 @@ const PreviewProfile = () => {
       {/* Images */}
       <div className="mt-6 grid grid-cols-4 gap-2">
         <div className="col-span-4 md:col-span-3">
-          {/* <img src={PreviewProfileImg} alt="PreviewProfile" className="w-full h-96 object-cover rounded-lg" /> */}
+          <img src={PreviewProfileImg} alt="PreviewProfile" className="w-full h-96 object-cover rounded-lg" />
         </div>
         <div className="hidden md:flex flex-col gap-2">
-          {/* <img src={vendorManagement003} alt="Side 1" className="w-full h-32 object-cover rounded-lg" />
+          <img src={vendorManagement003} alt="Side 1" className="w-full h-32 object-cover rounded-lg" />
           <img src={PreviewProfileImg} alt="Side 2" className="w-full h-32 object-cover rounded-lg" />
-          <img src={previewProfile003} alt="Side 3" className="w-full h-32 object-cover rounded-lg" /> */}
+          <img src={previewProfile003} alt="Side 3" className="w-full h-32 object-cover rounded-lg" />
         </div>
       </div>
 
