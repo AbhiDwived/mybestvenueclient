0d24b21a50d7fc5600aa3cfd851fa472cbd4b97b
AbhiDwivedi <abhidwivedi687@gmail.com>
2025-08-04 12:44:24 +0530

    refactor: remove unused profile and vendor pages

--- src/pages/Vendor/NotFound.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const NotFound = () => {
-  return (
-    <div className='text-3xl font-bold text-center text-gray-400'>  404 NotFound</div>
-  )
-}
-
-export default NotFound
